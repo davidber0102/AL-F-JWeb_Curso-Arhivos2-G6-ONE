aa752824a21176011b37cb389cdd9eaaa084b4d3
davidber0102 <davidbernaldiaz1@gmail.com>
2024-06-21 19:04:17 -0500

    commit5

--- ApiRest-java/api/src/main/java/com/med/voll/api/Controller/MedicoController.java
+++ ApiRest-java/api/src/main/java/com/med/voll/api/Controller/MedicoController.java
@@ -1,6 +1,9 @@
 package com.med.voll.api.Controller;
 
 import com.med.voll.api.Medico.DatoRegistroMedico;
+import com.med.voll.api.Medico.Medico;
+import com.med.voll.api.Medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,10 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/medicos")
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository medicoRepository;
+
     @PostMapping
-    public void registarMedicos(@RequestBody DatoRegistroMedico datosregistroMedico) {
-        System.out.println("El requeste llego correctamente");
-        System.out.println(datosregistroMedico);
+    public void registarMedicos(@RequestBody DatoRegistroMedico datosRegistroMedico) {
+        //System.out.println("El requeste llego correctamente");
+        //System.out.println(datosRegistroMedico);
+        medicoRepository.save(new Medico(datosRegistroMedico));
+
 
     }
 }
---
+++ ApiRest-java/api/src/main/java/com/med/voll/api/Medico/Medico.java
@@ -0,0 +1,33 @@
+package com.med.voll.api.Medico;
+
+import com.med.voll.api.Direccion.Direccion;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "medicos")
+@Entity(name = "Medico")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Medico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String email;
+    private String documento;
+    @Enumerated(EnumType.STRING)
+    private Especialidad especialidad;
+    @Embedded
+    private Direccion direccion;
+
+    public Medico(DatoRegistroMedico datosRegistroMedico) {
+        this.nombre = datosRegistroMedico.nombre();
+        this.email = datosRegistroMedico.email();
+        this.documento = datosRegistroMedico.documento();
+        this.especialidad = datosRegistroMedico.especialidad();
+        this.direccion = new Direccion(datosRegistroMedico.direccion());
+    }
+}
